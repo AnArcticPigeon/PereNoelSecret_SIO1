bf000656320b598371e757c7274064201851ed17
Adrien Burnett <an.arctic.pigeon@gmail.com>
2023-01-04 00:04:26 +0100

    Update math-pere-noel-secret.py

--- math-pere-noel-secret.py
+++ math-pere-noel-secret.py
@@ -2,7 +2,7 @@ import random,math
 
 
 
-#nb_personne = int(input("Saisir le nombre de personne présente a la soirée: "))
+nb_personne = int(input("Saisir le nombre de personne présente a la soirée: "))
 
 
 
@@ -37,64 +37,74 @@ def subfactorial(n):
     else:
         return (n-1) * (subfactorial(n-1) + subfactorial(n-2))
 
-print(subfactorial(6))
+#print(subfactorial(6))
 
-'''
+
+
+
+
+
+
+
+
 def permutation(nb_personne):
     nb_permutation = 1
 
     for i in range(nb_personne):
         nb_permutation = nb_permutation * (i+1)
-    print("Il y a",nb_permutation,"possible")
+    print("Il y a",nb_permutation,"permutation possible pour ",nb_personne,"invitées")
     return nb_permutation
-nb_permutation = permutation(nb_personne)
-
 
 
 def tirage_compliquer(nb_personne,nb_permutation):
     l_permutation = []
-    t=1
-    while(len(l_permutation) < nb_permutation):
+    l_derangement = []
+    numTour=1
+    #boucle tant que l'on a pas fait toute les possibilitée de permutation possible
+    while(len(l_derangement) < nb_permutation):
         
-        print("tour:",t)
+        print("tour:",numTour)
         l_cadeau2 = []
         l_cadeau_tire = []
-        y = True
-        #boucle pour chaque persone
-        while(y == True):
-            for i in range(nb_personne):
-                
-                p = True
-                nb = 0
-                l_cadeau = []
-                
-                #boucle tant que le cadeau tire est celui de la persone ou un deja tire
-                while(p == True):
-                    nb = random.randrange(1,nb_personne+1)
-                    if(nb in l_cadeau_tire or nb == i+1):
-                        p = True
-                    else:
-                        p = False
-                    if(nb == nb_personne and nb not in l_cadeau_tire):
-                        y = True
-                        break
-                    else:
-                        y = False
-                l_cadeau.append(i+1)
-                l_cadeau_tire.append(nb)
-                l_cadeau.append(nb)
-                l_cadeau2.append(l_cadeau)
-                print(i+1,"a obtenue le cadeau de",nb)
-                i=i+1
-            
-        if(l_cadeau2 not in l_permutation):
-            l_permutation.append(l_cadeau2)
+        
+        
 
-        t=t+1
-        print("len(l_permutation)=",len(l_permutation))
-    print(l_permutation)
-    print(len(l_permutation))
-tirage_compliquer(nb_personne,nb_permutation)
-'''
+        #boucle donant un cadeau unique a chaque persone
+        for i in range(nb_personne):
+            p = True
+            nb = 0
+            l_cadeau = []
+            
+            #boucle tant que le cadeau tire est celui de la persone ou un deja tire
+            while(p == True):
+                nb = random.randrange(1,nb_personne+1)
+                if(nb in l_cadeau_tire or nb == i+1):
+                    break
+                else:
+                    p = False
+                '''
+                if(nb == nb_personne and nb not in l_cadeau_tire):
+                    y = True
+                    break
+                else:
+                    y = False
+                '''
+
+            l_cadeau_tire.append(nb)
+            l_cadeau.append(i+1)
+            l_cadeau.append(nb)
+            l_cadeau2.append(l_cadeau)
+            print(i+1,"a obtenue le cadeau de",nb)
+            i=i+1
+            
+        if(l_cadeau2 not in l_derangement):
+            l_derangement.append(l_cadeau2)
+
+        numTour = numTour + 1
+    print("Il y a ",len(l_permutation),"permutation possible")
+    print("Il y a ",len(l_derangement),"derangement possible")
+    print(l_derangement)
+tirage_compliquer(nb_personne,permutation(nb_personne))
+
 
 
