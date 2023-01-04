fb4063910640e6e6fb20bb723538cfee57c3ac15
Adrien Burnett <an.arctic.pigeon@gmail.com>
2023-01-04 23:33:26 +0100

    Update math-pere-noel-secret.py

--- math-pere-noel-secret.py
+++ math-pere-noel-secret.py
@@ -1,5 +1,6 @@
-import random,math,time
+import random,math,time,sys
 
+sys.setrecursionlimit(2000)
 
 """
 Le but de ce programe est de calculer le nombre de dérangement possible pour un nombre n d'element
@@ -47,7 +48,7 @@ def tirage_compliquer(nb_personne,nb_permutation,permu):
         #si le derangement actuelle n'est pas deja present dans la liste de tout les derangement, alors on l'ajoute
         if(binarySearch(l_derangement, 0, len(l_derangement)-1, l_derangement_u) == -1):
             l_derangement.append(l_derangement_u)
-            l_derangement.sort()
+            quickSort(l_derangement, 0, len(l_derangement) - 1)
 
 
         numTest = numTest + 1
@@ -120,6 +121,67 @@ def binarySearch(arr, l, r, x):
     # If we reach here, then the element
     # was not present
     return -1
+
+
+
+
+# Python program for implementation of Quicksort Sort
+ 
+# This implementation utilizes pivot as the last element in the nums list
+# It has a pointer to keep track of the elements smaller than the pivot
+# At the very end of partition() function, the pointer is swapped with the pivot
+# to come up with a "sorted" nums relative to the pivot
+ 
+ 
+# Function to find the partition position
+def partition(array, low, high):
+ 
+    # choose the rightmost element as pivot
+    pivot = array[high]
+ 
+    # pointer for greater element
+    i = low - 1
+ 
+    # traverse through all elements
+    # compare each element with pivot
+    for j in range(low, high):
+        if array[j] <= pivot:
+ 
+            # If element smaller than pivot is found
+            # swap it with the greater element pointed by i
+            i = i + 1
+ 
+            # Swapping element at i with element at j
+            (array[i], array[j]) = (array[j], array[i])
+ 
+    # Swap the pivot element with the greater element specified by i
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+ 
+    # Return the position from where partition is done
+    return i + 1
+ 
+# function to perform quicksort
+ 
+ 
+def quickSort(array, low, high):
+    if low < high:
+ 
+        # Find pivot element such that
+        # element smaller than pivot are on the left
+        # element greater than pivot are on the right
+        pi = partition(array, low, high)
+ 
+        # Recursive call on the left of pivot
+        quickSort(array, low, pi - 1)
+ 
+        # Recursive call on the right of pivot
+        quickSort(array, pi + 1, high)
+ 
+ 
+
+ 
+
+
       
 tirage_compliquer(nb_personne,permutation(nb_personne),permu)
 
